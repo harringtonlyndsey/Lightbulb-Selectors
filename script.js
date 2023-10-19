462f9724288687f770e3bb3f313a7329419ecb63
harringtonlyndsey <lyndsey.harrington@vodafone.com>
2023-10-19 10:48:28 +0100

    changes made

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
-// Write your code here
+const one = document.querySelector("#lightbulb1");
+const two = document.querySelector("#lightbulb2");
+const three = document.querySelector("#lightbulb3");
+const four = document.querySelector("h3");
+let count = 0;
+
+new.addEventListener("click", function(){
+    count+=1;
+    subtitle.textContent = `You've clicked the lights ${count} times`;
+    bulb.classList.toggle("item lightbulb active");
+    bulb.classList.toggle("item lightbulb");
+})
